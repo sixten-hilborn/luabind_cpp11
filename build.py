e74a46193c6b9947cf5783e3862891b9e6efcbc4
Sixten Hilborn <hilborn90@gmail.com>
2017-01-21 10:07:39 +0100

    Remove some non-C++14 compilers from CI

--- build.py
+++ build.py
@@ -4,5 +4,12 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="luabind11:shared", pure_c=False)
+    # Remove compiler versions that don't support used C++14 features
+    builder.builds = [
+        [settings, options]
+        for settings, options in builder.builds
+        if not (settings["compiler"] == "Visual Studio" and int(settings["compiler.version"]) < 13)
+        or not (settings["compiler"] == "gcc" and float(settings["compiler.version"]) < 4.8)
+    ]
     builder.run()
 
