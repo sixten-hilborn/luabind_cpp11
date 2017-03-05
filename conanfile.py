febb8f0cc5ef51f335e5b625a057da34bee5840c
Sixten Hilborn <hilborn90@gmail.com>
2017-03-05 16:35:15 +0100

    Use new cmake helper in conan

--- conanfile.py
+++ conanfile.py
@@ -24,17 +24,14 @@ class Luabind11Conan(ConanFile):
 
 
     def build(self):
-        self.makedir('_build')
         cmake = CMake(self.settings)
-        cd_build = 'cd _build'
-        shared_option = 1 if self.options.shared else 0
-        options = (
-            '-DCMAKE_INSTALL_PREFIX=../_build/install '
-            '-DBUILD_TEST=0 '
-            '-DBUILD_SHARED={0} ').format(shared_option)
-        build_options = ''
-        self.run_and_print('%s && cmake .. %s %s' % (cd_build, cmake.command_line, options))
-        self.run_and_print("%s && cmake --build . --target install %s %s" % (cd_build, cmake.build_config, build_options))
+        options = {
+            'CMAKE_INSTALL_PREFIX': '../_build/install',
+            'BUILD_TEST': False,
+            'BUILD_SHARED': self.options.shared
+        }
+        cmake.configure(self, defs=options, build_dir='_build')
+        cmake.build(self, target='install')
 
     def package(self):
         # Headers
@@ -54,13 +51,3 @@ class Luabind11Conan(ConanFile):
         self.cpp_info.libs = ['luabind11']
         if self.options.shared:
             self.cpp_info.defines = ['LUABIND_DYNAMIC_LINK']
-
-    def makedir(self, path):
-        if self.settings.os == "Windows":
-            self.run("IF not exist {0} mkdir {0}".format(path))
-        else:
-            self.run("mkdir {0}".format(path))
-
-    def run_and_print(self, command):
-        self.output.info(command)
-        self.run(command)
