a8dff04d96cdf0e1233e029fd2f8444319e8f26d
Sixten Hilborn <hilborn90@gmail.com>
2018-01-04 22:07:26 +0100

    Use bincrafters ci template

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/sixten-hilborn/luabind_cpp11.svg?branch=master)](https://travis-ci.org/sixten-hilborn/luabind_cpp11)
+[![Build Status](https://travis-ci.org/sixten-hilborn/luabind_cpp11.svg?branch=stable/0.9.2)](https://travis-ci.org/sixten-hilborn/luabind_cpp11)
 [![Build status](https://ci.appveyor.com/api/projects/status/1m35wuu55qp89a6d?svg=true)](https://ci.appveyor.com/project/sixten-hilborn/luabind-cpp11)
 
 luabind_cpp11
--- build.py
+++ build.py
@@ -1,15 +1,38 @@
-from conan.packager import ConanMultiPackager
-import platform
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from conan.packager import ConanMultiPackager
+from conans import tools
+import importlib
+import os
+
+
+def get_module_location():
+    repo = os.getenv("CONAN_MODULE_REPO", "https://raw.githubusercontent.com/bincrafters/conan-templates")
+    branch = os.getenv("CONAN_MODULE_BRANCH", "package_tools_modules")
+    return repo + "/" + branch
+
+    
+def get_module_name():
+    return os.getenv("CONAN_MODULE_NAME", "build_template_default")
+
+    
+def get_module_filename():
+    return get_module_name() + ".py"
+    
+    
+def get_module_url():
+    return get_module_location() + "/" + get_module_filename()
+
+    
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="luabind11:shared")
-    # Remove compiler versions that don't support used C++14 features
-    builder.builds = [
-        [settings, options, env_vars, build_requires]
-        for settings, options, env_vars, build_requires in builder.builds
-        if  not (settings["compiler"] == "Visual Studio" and int(settings["compiler.version"]) < 13)
-        and not (settings["compiler"] == "gcc" and float(settings["compiler.version"]) < 4.8)
-    ]
+    
+    tools.download(get_module_url(), get_module_filename(), overwrite=True)
+    
+    module = importlib.import_module(get_module_name())
+    
+    builder = module.get_builder()
+    
     builder.run()
 
+    
